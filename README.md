d771d8220beb83310324171a63d08d04b69d2f90
Abraham Eguiarte <abraham.lopez5085@alumnos.udg.mx>
2022-02-16 16:14:58 -0600

    Se han agregado un archivo README.md y se hizo una ligera actualización al código de geek out master

---
+++ README.md
@@ -0,0 +1,3 @@
+Repositorio creado con la finalidad de llevar un seguimiento de aquellos códigos que he desarrollado por diversión.
+Todos los códigos que se encuentren aquí son hechos sin fines de lucro y con el mero fin de practicar y pasar mi tiempo
+libre de vez en cuando, recreando juegos de mesa por el momento.
--- geek_out_master.py
+++ geek_out_master.py
@@ -23,22 +23,80 @@ class Dados():
 	def __len__(self):
 		return len(self.all_dice)
 
+	def remover_uno(self):
+		return self.all_dice.pop(0)
+
+class Dados_inactivos():
+	def __init__(self):
+		self.dados_inactivos = []
+	
+	def introducir_dados(self, dados):
+		self.dados_inactivos.append(dados)
+	
+	def __len__(self):
+		return len(self.dados_inactivos)
+
+#class Dados_activos():
+#	def __init__(self):
+#		self.dados_activos = []
+	
+#	def introducir_dados(self, dados):
+#		self.dados_activos.extend(dados)
+	
+#	def __len__(self):
+#		return len(self.dados_activos)
+
 dados = Dados()
+contenedor_dados_inactivos = Dados_inactivos()
 index = 0
 for obj in range(10):
 	random.shuffle(dados.all_dice[index].valor)
 	index+=1
-print(dados.all_dice[0].valor[0::])
-print(dados.all_dice[1].valor[0::])
-print(dados.all_dice[2].valor[0::])
-print(dados.all_dice[3].valor[0::])
-print(dados.all_dice[4].valor[0::])
-print(dados.all_dice[5].valor[0::])
-print(dados.all_dice[6].valor[0::])
-print(dados.all_dice[7].valor[0::])
-print(dados.all_dice[8].valor[0::])
-print(dados.all_dice[9].valor[0::])
+print(len(contenedor_dados_inactivos))
+print(len(dados.all_dice))
+aux = 0
+print("Dados antes de enviar tres a inactivos")
+while(True):
+	if aux <= 9:
+		print(dados.all_dice[aux].valor[0::])
+		aux+=1
+	else:
+		aux = 0
+		break
+while(True):
+	if len(contenedor_dados_inactivos) != 3:
+		contenedor_dados_inactivos.introducir_dados(dados.remover_uno())
+	else:
+		break
+print(len(contenedor_dados_inactivos))
+print(len(dados.all_dice))
+print("Dados después de enviar tres a inactivos")
+while(True):
+	if aux < len(dados.all_dice):
+		print(dados.all_dice[aux].valor[0::])
+		aux+=1
+	else:
+		aux = 0
+		break
+print("Dados inactivos")
+while(True):
+	if aux < len(contenedor_dados_inactivos):
+		print(contenedor_dados_inactivos.dados_inactivos[aux].valor[0::])
+		aux+=1
+	else:
+		break
 
-cara = random.randint(0, 5)
-print(cara)
-print(valor_cara[dados.all_dice[0].valor[cara]])
+#print(dados.all_dice[0].valor[0::])
+#print(dados.all_dice[1].valor[0::])
+#print(dados.all_dice[2].valor[0::])
+#print(dados.all_dice[3].valor[0::])
+#print(dados.all_dice[4].valor[0::])
+#print(dados.all_dice[5].valor[0::])
+#print(dados.all_dice[6].valor[0::])
+#print(dados.all_dice[7].valor[0::])
+#print(dados.all_dice[8].valor[0::])
+#print(dados.all_dice[9].valor[0::])
+
+#cara = random.randint(0, 5)
+#print(cara)
+#print(valor_cara[dados.all_dice[0].valor[cara]])
